620d07c6491568323161884c4a2c5d50009dc7db
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-14 10:02:28 -0500

    working esmock

---
+++ test/math-test.mjs
@@ -0,0 +1,19 @@
+import { it, mock } from 'node:test'
+import assert from 'node:assert/strict'
+import esmock from 'esmock'
+import { calculate } from './calculator.mjs'
+
+it('adds two numbers', () => {
+  assert.equal(calculate('+', 2, 3), 5)
+})
+
+it('adds two numbers (mocks add)', async () => {
+  const add = mock.fn(() => 20)
+  const { calculate } = await esmock('./calculator.mjs', {
+    './math.mjs': {
+      add,
+    },
+  })
+  assert.equal(calculate('+', 2, 3), 20)
+  assert.deepEqual(add.mock.calls[0].arguments, [2, 3])
+})
---
+++ test/math.mjs
@@ -0,0 +1,4 @@
+export const add = (a, b) => {
+  console.log('adding %d to %d', a, b)
+  return a + b
+}
